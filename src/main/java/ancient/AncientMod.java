d3330b6d1b9cb09a33501e9c86e6f29640cd3995
notalizard <gcb140330@utdallas.edu>
2019-01-25 17:56:52 -0600

    Started mod

---
+++ src/main/java/ancient/AncientMod.java
@@ -0,0 +1,15 @@
+package ancient;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+
+@SpireInitializer
+public class AncientMod {
+
+    public AncientMod(){
+
+    }
+
+    public static void initialize(){
+        new AncientMod();
+    }
+}
